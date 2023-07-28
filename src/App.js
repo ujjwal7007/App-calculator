01511b19e585d0eb2103d3449d326673832224c8
Ujjwal Gupta <125856546+ujjwal7007@users.noreply.github.com>
2023-07-28 13:11:25 +0530

    incomplete calculator

--- src/App.js
+++ src/App.js
@@ -2,133 +2,87 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [current, setCurrent] = useState("");
-  const [previous, setPrevious] = useState("");
-  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+  const [total, setTotal] = useState("");
 
-  const handleClick = (e) => {
-    const value = e.target.getAttribute("name");
-    setCurrent(current + value);
+  const back = () => {
+    setResult(result.slice(0, -1));
   };
 
-  const backspace = () => {
-    setCurrent(current.slice(0, -1));
+  const calculate = (e) => {
+    setResult(result.concat(e.target.name));
   };
 
-  const operationHandle = (e) => {
-    if (current === "") return;
-    if (previous !== "") {
-      let value = compute();
-      setPrevious(value);
-    } else {
-      setPrevious(current);
-    }
-    setCurrent("");
-    setOperation(e.target.getAttribute("name"));
-  };
-
-  const calculate = () => {
-    let value = compute();
-    if (value === undefined || value === null) return;
-    setCurrent(value);
-    setPrevious("");
-    setOperation("");
-  };
-
-  const compute = () => {
-    let result;
-    let previousNumber = parseFloat(previous);
-    let currentNumber = parseFloat(current);
-
-    // console.log("previous:", previousNumber);
-    // console.log("current:", currentNumber);
-    // console.log("operation:", operation);
-
-    switch (operation) {
-      case "*":
-        result = previousNumber * currentNumber;
-        break;
-      case "/":
-        result = previousNumber / currentNumber;
-        break;
-      case "%":
-        result = previousNumber % currentNumber;
-        break;
-      case "-":
-        result = previousNumber - currentNumber;
-        break;
-      case "+":
-        result = previousNumber + currentNumber;
-        break;
-      default:
-        return;
-    }
-    // console.log("result:", result);
-    return result;
+  const equal = () => {
+    setResult(eval(result).toString());
+    setTotal(result);
   };
 
   return (
     <div className="container">
       <form className="left">
-        <input type="text" value={`${previous} ${operation}`} />
-        <div>
-          <input className="bottom" type="text" value={current} />
-        </div>
+        <input type="text" value={total} />
+        <input
+          className="bottom"
+          type="text"
+          value={result}
+          onChange={(e) => setResult(e.target.value)}
+        />
       </form>
       <div className="right">
         <h1>Vertical Calculator</h1>
         <div>
-          <button onClick={backspace} id="backspace">
+          <button onClick={back} id="backspace">
             Clear
           </button>
-          <button onClick={calculate} id="result">
+          <button onClick={equal} id="result">
             =
           </button>
         </div>
         <div className="keypad">
-          <button name="*" onClick={operationHandle}>
+          <button name="X" onClick={calculate}>
             X
           </button>
-          <button name="/" onClick={operationHandle}>
+          <button name="/" onClick={calculate}>
             /
           </button>
-          <button name="%" onClick={operationHandle}>
+          <button name="%" onClick={calculate}>
             %
           </button>
-          <button name="1" onClick={handleClick}>
+          <button name="1" onClick={calculate}>
             1
           </button>
-          <button name="2" onClick={handleClick}>
+          <button name="2" onClick={calculate}>
             2
           </button>
-          <button name="3" onClick={handleClick}>
+          <button name="3" onClick={calculate}>
             3
           </button>
-          <button name="4" onClick={handleClick}>
+          <button name="4" onClick={calculate}>
             4
           </button>
-          <button name="5" onClick={handleClick}>
+          <button name="5" onClick={calculate}>
             5
           </button>
-          <button name="6" onClick={handleClick}>
+          <button name="6" onClick={calculate}>
             6
           </button>
-          <button name="7" onClick={handleClick}>
+          <button name="7" onClick={calculate}>
             7
           </button>
-          <button name="8" onClick={handleClick}>
+          <button name="8" onClick={calculate}>
             8
           </button>
-          <button name="9" onClick={handleClick}>
-            9
+          <button name="9" onClick={calculate}>
+            9{calculate}
           </button>
-          <button name="-" onClick={operationHandle}>
+          <button name="-" onClick={calculate}>
             -
           </button>
-          <button name="0" onClick={handleClick}>
+          <button name="0" onClick={calculate}>
             0
           </button>
-          <button name="+" onClick={operationHandle}>
+          <button name="+" onClick={calculate}>
             +
           </button>
         </div>
